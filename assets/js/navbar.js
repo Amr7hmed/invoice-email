9ba11bc2c5bb7b3ad2393fe3675c4224fd2c0183
Amr7hmed <amr7hmed@gmail.com>
2022-06-02 15:05:03 +0200

    Update Data Project

---
+++ assets/js/navbar.js
@@ -0,0 +1,23 @@
+$('.select-country').flagStrap({
+	countries: {
+		"US": "USD",
+		"AU": "AUD",
+		"CA": "CAD",
+		"SG": "SGD",
+		"GB": "GBP",
+		"EG": "EGP",
+	},
+	buttonSize: "btn-sm",
+	buttonType: "btn-info",
+	labelMargin: "10px",
+	scrollable: false,
+	scrollableHeight: "350px"
+});
+
+$('#advanced').flagStrap({
+	buttonSize: "btn-lg",
+	buttonType: "btn-primary",
+	labelMargin: "20px",
+	scrollable: false,
+	scrollableHeight: "350px"
+});
